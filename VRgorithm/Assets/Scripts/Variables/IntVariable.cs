fe77e82f406c4862d52303c3eab309642c85912d
hanabzu <hanthird@gmail.com>
2022-05-13 15:09:59 +0900

    Create: VariableManager

--- VRgorithm/Assets/Scripts/Variables/IntVariable.cs
+++ VRgorithm/Assets/Scripts/Variables/IntVariable.cs
@@ -1,15 +1,22 @@
 using UnityEngine;
-
+using System.Collections.Generic;
 
 public class IntVariable : MonoBehaviour, IVariable {
-    public int Value { get; set; }
+    public Stack<int> stack;
+
+    public int Value { get; set;} = 0;
 
     public string Name {
         get { return Name; }
         set { Name = value; }
     }
 
-    void start() {
+    void Start() {
+        stack.Push(Value);
+    }
 
+    public void undo() {
+        stack.Pop();
+        Value = stack.Peek();
     }
 }
