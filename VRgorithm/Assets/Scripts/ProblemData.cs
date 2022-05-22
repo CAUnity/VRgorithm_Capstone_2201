74bd75dd403fae1abc7eeccd90759554745bffc6
SibaDoge1 <rladus753159@gmail.com>
2022-05-22 21:49:34 +0900

    [feat]: add 문제 데이터 글로벌클래스
    - 문제 데이터를 저장하고있는 ProblemData 클래스 추가

---
+++ VRgorithm/Assets/Scripts/ProblemData.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ProblemData
+{
+    public static ArrayList inputs = new ArrayList() {10, 1, 3, 5, 15};
+    public static ArrayList outputs = new ArrayList() {15};
+    public static string decription = "받은 값 중에서 가장 큰 값을 결과변수에 저장하세요";
+
+}
